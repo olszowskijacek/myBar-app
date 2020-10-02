5d17bff9c0e32673c030bb86aa4a6db6eaebe50e
Jacek Olszowski <jacek.olszowski1@gmail.com>
2020-10-02 18:01:58 +0200

    add Header compontent

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 
+import Header from './components/Header';
+
 function App() {
   return (
     <div className="App">
-      Hello
+      <Header title='Napijmy się!'/>
     </div>
   );
 }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './style.css';
+
+
+const Header = ({title}) => (
+    <h1>{title}</h1>
+)
+
+export default Header;
